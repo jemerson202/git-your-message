465e698e1e93214f3ea3d32ec195ec20c690daf9
Brigitta <baltwo@gmail.com>
2017-08-07 17:25:30 -0600

    added answer to question about graceful degradation and progressive enhancement

--- mike-dreiling/tests.js
+++ mike-dreiling/tests.js
@@ -3,3 +3,11 @@ describe('Return The Average Of A Set Of Numbers In An Array!!!!', function() {
     expect(index.arrAvg([5, 22, 27, 8, 100])).to.be.a('number');
   });
 });
+
+function arrAvg() {
+
+
+}
+
+
+Look into Jest as a testing framework
